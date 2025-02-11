931813ce0914bf60adb26b16c3b956c354ddccbc
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2025-02-10 23:01:47 -0500

    added some HSD symbols

--- NA/SymbolMaps/ZZStriper.py
+++ NA/SymbolMaps/ZZStriper.py
@@ -1,6 +1,7 @@
 #goes through all the maps, removing any functions that start with
 #"zz_" since those are whatever addresses and not useful to us.
 
+"""
 #removes the zz_
 def process_file(filename):
     lines = ""
@@ -11,16 +12,23 @@ def process_file(filename):
                 lines = lines + line
 
     return lines
+"""
+#removes the null pointers
+def process_file(filename):
+    lines = ""
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            if "fp_FUN" not in line:
+                lines = lines + line
+
+    return lines
+
+
 
 
 #goes through all the maps and store
-filename = "kar.map"
+filename = "CleanedKAR.map"
 lines = process_file(filename)
-filename = "OtherKAR.map"
-lines = lines + '\n' + process_file(filename)
-filename = "Uber.map"
-lines = lines + '\n' + process_file(filename)
-
 #writes to a new file
-with open("CleanedKAR.map", 'w', encoding='utf-8') as outfile:
+with open("CleanedKAR2.map", 'w', encoding='utf-8') as outfile:
     outfile.writelines(lines)
